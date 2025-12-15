faa3070a2381c84ae873fe5c0a24c380f995ea72
helpdesk - pc <contatoluansantos7@gmail.com>
2025-12-15 10:37:52 -0300

    adicionando comentários explicativos na estrutura do usuário

--- internal/models/user.go
+++ internal/models/user.go
@@ -1,10 +1,19 @@
 package models
 
+//Importação do pacote time para manipulação de datas e horas
 import "time"
 
+//Definição da estrutura User com campos ID, Name, Email e CreatedAT
 type User struct {
 	ID        int       `json:"id"`
 	Name      string    `json:"name"`
 	Email     string    `json:"email"`
 	CreatedAT time.Time `json:"created_at"`
+}
+
+//Declaração de uma variável Users que é uma fatia de User, inicializada com alguns dados de exemplo(Banco de dados local)
+var Users = []User{
+	{ID: 1, Name: "Alice", Email: "alice@example.com", CreatedAT: time.Now()},
+	{ID: 2, Name: "Bob", Email: "bob@example.com", CreatedAT: time.Now()},
+	{ID: 3, Name: "Charlie", Email: "charlie@example.com", CreatedAT: time.Now()},
 }
