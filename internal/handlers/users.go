d03e99725e54084772751d2f012c90e6e968d2e6
helpdesk - pc <contatoluansantos7@gmail.com>
2025-12-15 14:28:14 -0300

    ajustes nos handlers e inclusão do getbyid no metodo getuser

--- internal/handlers/users.go
+++ internal/handlers/users.go
@@ -12,32 +12,43 @@ import (
 
 func GetUsers(w http.ResponseWriter, r *http.Request) {
 	log.Println("Buscando usuários")
+	idStr := r.URL.Query().Get("id")
+	if idStr != "" {
+		GetUserByID(w, r)
+		return
+	}
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(models.Users)
+	log.Println("Usuários retornados com sucesso")
 }
 
 func CreateUser(w http.ResponseWriter, r *http.Request) {
+	log.Println("Criando novo usuário")
 	var user models.User
 	if err := json.NewDecoder(r.Body).Decode(&user); err != nil {
+		log.Println("Erro ao decodificar o corpo da requisição:", err)
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 	user.ID = len(models.Users) + 1
-	user.CreatedAT = time.Now()
+	user.CreatedAt = time.Now()
 	models.Users = append(models.Users, user)
+	log.Println("Usuário criado com sucesso:", user)
 
-	w.Header().Set("Content-type", "application/json")
+	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusCreated)
 	json.NewEncoder(w).Encode(user)
 }
 
 func GetUserByID(w http.ResponseWriter, r *http.Request) {
+	log.Println("Buscando usuário por ID")
 	idStr := r.URL.Query().Get("id")
 	id, _ := strconv.Atoi(idStr)
 	for _, user := range models.Users {
 		if user.ID == id {
 			w.Header().Set("Content-Type", "application/json")
 			json.NewEncoder(w).Encode(user)
+			log.Println("Usuário retornado com sucesso:", user)
 			return
 		}
 	}
--- internal/models/user.go
+++ internal/models/user.go
@@ -8,12 +8,12 @@ type User struct {
 	ID        int       `json:"id"`
 	Name      string    `json:"name"`
 	Email     string    `json:"email"`
-	CreatedAT time.Time `json:"created_at"`
+	CreatedAt time.Time `json:"created_at"`
 }
 
 //Declaração de uma variável Users que é uma fatia de User, inicializada com alguns dados de exemplo(Banco de dados local)
 var Users = []User{
-	{ID: 1, Name: "Alice", Email: "alice@example.com", CreatedAT: time.Now()},
-	{ID: 2, Name: "Bob", Email: "bob@example.com", CreatedAT: time.Now()},
-	{ID: 3, Name: "Charlie", Email: "charlie@example.com", CreatedAT: time.Now()},
+	{ID: 1, Name: "Alice", Email: "alice@example.com", CreatedAt: time.Now()},
+	{ID: 2, Name: "Bob", Email: "bob@example.com", CreatedAt: time.Now()},
+	{ID: 3, Name: "Charlie", Email: "charlie@example.com", CreatedAt: time.Now()},
 }
