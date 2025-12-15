8cf8dc435e12cf0a2ff81192b5468f8ca10e8c70
helpdesk - pc <contatoluansantos7@gmail.com>
2025-12-15 13:12:47 -0300

    implementando manipulador de usuários com suporte a GET e POST

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+
+	"projetinho/internal/handlers"
+)
+
+func usersHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "GET":
+		handlers.GetUsers(w, r)
+	case "POST":
+		handlers.CreateUser(w, r)
+	default:
+		http.Error(w, "Método não permitido", http.StatusMethodNotAllowed)
+	}
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	http.HandleFunc("/users", usersHandler)
+
+	log.Println("Servidor rodando em http://localhost:8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
